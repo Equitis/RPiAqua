0451f4c4f8f87eb3ae4aa794c39cacdb49ae9e52
Equitis <fuhr.foo@googlemail.com>
2015-09-10 12:09:14 +0200

    DB Connection beginn

---
+++ RPiAqua.DB/MySQL/MySqlConnector.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace RPiAqua.DB.MySQL
+{
+	public class MySqlConnector
+	{
+		private MySqlConnection _connection;
+
+		/// <summary>
+		/// Hoster of the URL
+		/// </summary>
+		public string Host { get; set; }
+		/// <summary>
+		/// Name of Database
+		/// </summary>
+		public string DbName { get; set; }
+		/// <summary>
+		/// Username to Connect
+		/// </summary>
+		public string Username { get; set; }
+		/// <summary>
+		/// Password to Connect
+		/// </summary>
+		public string Password { get; set; }
+
+		public MySqlConnector(string host)
+		{
+			Host = host;
+		}
+
+		public MySqlConnector(string host, string dbName)
+			: this(host)
+		{
+			DbName = dbName;
+		}
+
+		public MySqlConnector(string host, string dbName, string userName)
+			: this(host, dbName)
+		{
+			Username = userName;
+		}
+
+		public MySqlConnector(string host, string dbName, string userName, string passwd) :
+			this(host, dbName, userName)
+		{
+			Password = passwd;
+		}
+
+		/// <summary>
+		/// <para>Init the Connection to the Database</para>
+		/// <remarks>All Connection Settings has to be set</remarks>
+		/// </summary>
+		/// <returns>(bool) true when success</returns>
+		public bool Initialize()
+		{
+			bool bRet = false;
+
+			if (!string.IsNullOrEmpty(Host) && !string.IsNullOrEmpty(DbName) && !string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Password))
+			{
+
+				string connectionString = "SERVER=" + Host + ";" + "DATABASE=" + DbName + ";" + "UID=" + Username + ";" + "PASSWORD=" + Password + ";";
+
+				_connection = new MySqlConnection(connectionString);
+
+				if (_connection != null)
+					bRet = true;
+			}
+
+			return bRet;
+		}
+
+		/// <summary>
+		/// Open the Database Connection
+		/// </summary>
+		/// <returns>(true) If success</returns>
+		public bool Connect()
+		{
+			bool bRet = false;
+
+			try
+			{
+				if (_connection.State == ConnectionState.Closed)
+					_connection.Open();
+
+				if (_connection.State == ConnectionState.Open)
+					bRet = true;
+			}
+			catch (MySqlException ex)
+			{
+				bRet = false;
+			}
+
+			return bRet;
+		}
+
+		/// <summary>
+		/// Close the Database Connection
+		/// </summary>
+		/// <returns>(true) If success</returns>
+		public bool Disconnect()
+		{
+			bool bRet = false;
+
+			if (_connection.State == ConnectionState.Open)
+				_connection.Close();
+
+			if (_connection.State == ConnectionState.Closed)
+				bRet = true;
+
+			return bRet;
+		}
+	}
+}
---
+++ RPiAqua.DBTests/MySQL/MySqlConnectionTests.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RPiAqua.DB.MySQL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace RPiAqua.DB.MySQL.Tests
+{
+	[TestClass()]
+	public class MySqlConnectionTests
+	{
+		public const string Host = "localhost";
+		public const string Dbname = "RPiAqua";
+		public const string Username = "root";
+		public const string Passwd = "Runr4646";
+
+		[TestMethod]
+		public void MysqlConnectionHostTest()
+		{
+			MySqlConnector mySql = new MySqlConnector(Host);
+			Assert.AreEqual(mySql.Host, Host);
+		}
+
+		[TestMethod]
+		public void MysqlConnectionHostDbNameTest()
+		{
+			MySqlConnector mySql = new MySqlConnector(Host, Dbname);
+			Assert.AreEqual(mySql.Host, Host);
+			Assert.AreEqual(mySql.DbName, Dbname);
+		}
+
+		[TestMethod]
+		public void MysqlConnectionHostDbNameUserTest()
+		{
+			MySqlConnector mySql = new MySqlConnector(Host, Dbname,Username);
+			Assert.AreEqual(mySql.Host, Host);
+			Assert.AreEqual(mySql.DbName, Dbname);
+			Assert.AreEqual(mySql.Username, Username);
+		}
+
+		[TestMethod]
+		public void MysqlConnectionHostDbNameUserPwTest()
+		{
+			MySqlConnector mySql = new MySqlConnector(Host, Dbname, Username, Passwd);
+			Assert.AreEqual(mySql.Host, Host);
+			Assert.AreEqual(mySql.DbName, Dbname);
+			Assert.AreEqual(mySql.Username, Username);
+			Assert.AreEqual(mySql.Password, Passwd);
+		}
+
+		[TestMethod]
+		public void MySqlConnectionInitSuccessTest()
+		{
+			MySqlConnector mySql = new MySqlConnector(Host, Dbname, Username, Passwd);
+			Assert.IsTrue(mySql.Initialize());
+		}
+
+		[TestMethod]
+		public void MySqlConnectionInitMissingPasswdTest()
+		{
+			MySqlConnector mySql = new MySqlConnector(Host, Dbname, Username);
+			Assert.IsFalse(mySql.Initialize());
+		}
+
+		[TestMethod]
+		public void MySqlConnectionInitMissingPasswdUserTest()
+		{
+			MySqlConnector mySql = new MySqlConnector(Host, Dbname);
+			Assert.IsFalse(mySql.Initialize());
+		}
+
+		[TestMethod]
+		public void MySqlConnectionInitMissingPasswdUserDbNameTest()
+		{
+			MySqlConnector mySql = new MySqlConnector(Host);
+			Assert.IsFalse(mySql.Initialize());
+		}
+
+		[TestMethod]
+		public void MySqlConnectionConnectSuccess()
+		{
+			MySqlConnector mySql = new MySqlConnector(Host, Dbname, Username, Passwd);
+			mySql.Initialize();
+
+			Assert.IsTrue(mySql.Connect());
+		}
+
+		[TestMethod]
+		public void MySqlConnectionConnectWrongPw()
+		{
+			MySqlConnector mySql = new MySqlConnector(Host, Dbname, Username, "test");
+			mySql.Initialize();
+
+			Assert.IsFalse(mySql.Connect());
+		}
+
+		[TestMethod]
+		public void MySqlConnectionConnectWrongUser()
+		{
+			MySqlConnector mySql = new MySqlConnector(Host, Dbname, "Testuser", Passwd);
+			mySql.Initialize();
+
+			Assert.IsFalse(mySql.Connect());
+		}
+
+		[TestMethod]
+		public void MySqlConnectionConnectWrongDbName()
+		{
+			MySqlConnector mySql = new MySqlConnector(Host, "Foobar", Username, Passwd);
+			mySql.Initialize();
+
+			Assert.IsFalse(mySql.Connect());
+		}
+
+		[TestMethod]
+		public void MySqlConnectionConnectWrongHost()
+		{
+			MySqlConnector mySql = new MySqlConnector("local", Dbname, Username, Passwd);
+			mySql.Initialize();
+
+			Assert.IsFalse(mySql.Connect());
+		}
+
+		[TestMethod]
+		public void MySqlConnectionDisconnect()
+		{
+			MySqlConnector mySql = new MySqlConnector(Host, Dbname, Username, Passwd);
+			mySql.Initialize();
+
+			mySql.Connect();
+
+			Assert.IsTrue(mySql.Disconnect());
+		}
+
+		[TestMethod]
+		public void MySqlConnectionDisconnectWithoutConnect()
+		{
+			MySqlConnector mySql = new MySqlConnector(Host, Dbname, Username, Passwd);
+			mySql.Initialize();
+
+			Assert.IsTrue(mySql.Disconnect());
+		}
+	}
+}
